6e00d65ebd350fe90e08a0005279ad0a34c92feb
Burak YILDIRIM <burakq@gmail.com>
2025-04-14 16:48:26 +0300

    Güncelleme listesi genişletildi

--- pages/admin/apply_github_updates.php
+++ pages/admin/apply_github_updates.php
@@ -21,18 +21,50 @@ $output = [];
 try {
     // GitHub'dan dosyaları indir
     $files = [
+        // Kök dizin dosyaları
         'version.txt' => 'https://raw.githubusercontent.com/burakq/artwork/main/version.txt',
+        'index.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/index.php',
+        'verify.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/verify.php',
+        'contact.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/contact.php',
+        'for_sale.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/for_sale.php',
+        'login.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/login.php',
+        'logout.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/logout.php',
+        'image_proxy.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/image_proxy.php',
+
+        // Admin sayfaları
         'pages/admin/artwork_techniques.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_techniques.php',
         'pages/admin/artwork_edition_types.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_edition_types.php',
         'pages/admin/artwork_editions.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_editions.php',
         'pages/admin/artworks.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artworks.php',
+        'pages/admin/artwork_add.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_add.php',
+        'pages/admin/artwork_edit.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_edit.php',
+        'pages/admin/artwork_view.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_view.php',
+        'pages/admin/artwork_locations.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_locations.php',
+        'pages/admin/artwork_statuses.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/artwork_statuses.php',
         'pages/admin/customers.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/customers.php',
         'pages/admin/customer_add.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/customer_add.php',
+        'pages/admin/customer_edit.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/customer_edit.php',
+        'pages/admin/customer_view.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/customer_view.php',
         'pages/admin/orders.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/orders.php',
+        'pages/admin/order_view.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/order_view.php',
         'pages/admin/verification_logs.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/verification_logs.php',
         'pages/admin/users.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/users.php',
         'pages/admin/user_add.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/user_add.php',
-        'pages/admin/settings.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/settings.php'
+        'pages/admin/user_edit.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/user_edit.php',
+        'pages/admin/settings.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/settings.php',
+        'pages/admin/sidebar_menu.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/sidebar_menu.php',
+        'pages/admin/dashboard.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/dashboard.php',
+
+        // Template dosyaları
+        'pages/admin/templates/header.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/templates/header.php',
+        'pages/admin/templates/footer.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/templates/footer.php',
+        'pages/admin/templates/sidebar.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/pages/admin/templates/sidebar.php',
+
+        // Config dosyaları
+        'config/db.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/config/db.php',
+
+        // Include dosyaları
+        'includes/functions.php' => 'https://raw.githubusercontent.com/burakq/artwork/main/includes/functions.php'
     ];
 
     $context = stream_context_create([
